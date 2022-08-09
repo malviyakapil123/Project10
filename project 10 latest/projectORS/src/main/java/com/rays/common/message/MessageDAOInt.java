3afe5702985c9e408a9012ecdab41d769854ba30
malviyakapil123 <kmalviya30@gmail.com>
2022-08-09 18:11:03 +0530

    p

---
+++ project 10 latest/projectORS/src/main/java/com/rays/common/message/MessageDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.common.message;
+
+import com.rays.common.BaseDAOInt;
+
+/**
+ * Role DAO interface.
+ * @author Kapil Malviya
+ */
+public interface MessageDAOInt extends BaseDAOInt<MessageDTO> {
+
+}
---
+++ project 10 latest/projectORS/src/main/java/com/rays/dao/RoleDAOInt.java
@@ -0,0 +1,12 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.RoleDTO;
+
+/**
+ * Role DAO interface.
+ * @author Kapil Malviya
+ */
+public interface RoleDAOInt extends BaseDAOInt<RoleDTO> {
+
+}
